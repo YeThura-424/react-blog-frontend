4cf48d52a2ef9e3d5f659ca8a3d21d2ac708e8be
YeThura-424 <rathanpoudle.26@gmail.com>
2024-04-18 17:37:14 +0630

    component structure

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const Footer = () => {
+  return (
+    <Fragment>
+      <div>Hello Footer</div>
+    </Fragment>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <div>Hello World</div>
+    </Fragment>
+  );
+};
+
+export default Header;
