5bf9cad070dc34b19040b6f74d8737ea91853a81
YeThura-424 <rathanpoudle.26@gmail.com>
2024-04-19 23:58:29 +0630

    layout update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
-// import "./App.css";
 import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import route from "./router";
 function App() {
   return (
     <Fragment>
+      <Header />
       <Home />
+      <Footer />
     </Fragment>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,14 @@
 import React, { Fragment } from "react";
-import Header from "../components/Header";
 import Slider from "../components/Slider";
 import FreshStories from "../components/FreshStories";
 import TrendingPosts from "../components/TrendingPosts";
-import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <Fragment>
-      <Header />
       <Slider />
       <FreshStories />
       <TrendingPosts />
-      <Footer />
     </Fragment>
   );
 };
