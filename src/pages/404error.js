86ee987a41c663986355a2f30c67127c00ca7fa4
YeThura-424 <rathanpoudle.26@gmail.com>
2024-04-27 22:20:50 +0630

    refector

--- src/pages/404error.js
+++ src/pages/404error.js
@@ -1,28 +1,30 @@
 import React, { Fragment } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
     <Fragment>
       <Header />
-      <section class="page-banner-section">
-        <div class="container">
+      <section className="page-banner-section">
+        <div className="container">
           <h1>Page Could Not Be Found</h1>
         </div>
       </section>
 
-      <section class="error-section">
-        <div class="container">
-          <div class="error-box">
+      <section className="error-section">
+        <div className="container">
+          <div className="error-box">
             <h1>404</h1>
             <h2>Page Not Found</h2>
             <p>
               We are sorry. But the page you are looking for cannot be found.{" "}
             </p>
-            <a class="button-one" href="index.html">
+
+            <Link className="button-one" to="/">
               Go To HomePage
-            </a>
+            </Link>
           </div>
         </div>
       </section>
