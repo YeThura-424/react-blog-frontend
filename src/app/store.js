e4cd82f29ffc2a2ce4266efe021c5065b8a03875
YeThura-424 <rathanpoudle.26@gmail.com>
2024-05-17 22:26:38 +0630

    non serialization data

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore} from "@reduxjs/toolkit";
 import postReducer from "../features/posts/postSlice";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 export const store = configureStore({
   reducer: {
     data: postReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
